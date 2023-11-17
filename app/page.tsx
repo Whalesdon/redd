ac64c3f44b3eb5724d2c6e48db19a52ccfe94c69
Rahdeg <walett95@gmail.com>
2023-11-17 10:52:56 +0100

    ee

--- app/page.tsx
+++ app/page.tsx
@@ -26,8 +26,8 @@ export default function Home() {
       <div className=" mt-20  flex items-center justify-center relative w-[420px] h-[350px]">
         <Image alt="" src='/card.png' fill style={backgroundImageStyle} />
         <div className=" absolute flex flex-col items-center justify-start top-5">
-          <p className=" font-semibold  text-2xl sm:text-2xl lg:text-2xl  1xl:text-4xl 3xl:text-5xl text-[#c4c4c4]">Opt-In pool</p>
-          <p className=" text-center px-14 mt-3 text-[#a09696]">Submit the transaction below to opt-in to the CULTmander pool.Note: it is required to have $250 RVLT staked at the time of opting in. Unstaking any amount will remove you from the pool.</p>
+          <p className=" font-semibold  text-2xl sm:text-2xl lg:text-2xl  1xl:text-4xl 3xl:text-5xl text-gray-500">Opt-In pool</p>
+          <p className=" text-center px-14 mt-3 text-gray-600">Submit the transaction below to opt-in to the CULTmander pool.Note: it is required to have $250 RVLT staked at the time of opting in. Unstaking any amount will remove you from the pool.</p>
           <button className="relative gap-2 px-2 flex items-center justify-center 3xl:h-14 3xl:w-64 3xl:text-4xl 3xl:mt-3 1xl:h-12 1xl:w-56 1xl:text-3xl 1xl:mt-3  border w-32 lg:h-9 h-7 m-auto rounded-xl mt-5 lg:text-xl text-lg font-Nixie bg-buttonBg border-gray-600 font-medium  bg-[#c4c4c4]">Submit</button>
 
         </div>
@@ -35,7 +35,7 @@ export default function Home() {
       </div>
       <div className=" flex items-center justify-center mt-0 relative">
         <Image alt="dd" src="/smallPaperCover.svg" width={150} height={150} className="" />
-        <div className="  -top-5  text-[#a09696] font-serif flex flex-col 3xl:w-108 2xl:w-80 xl:w-60 3xl:h-36 2xl:h-28 xl:h-20 sm:w-56 w-36 absolute items-center justify-evenly sm:h-16 h:10 sm:mt-8 mt-9"><p className="3xl:text-4xl 2xl:text-3xl  text-md sm:text-xl font-Nixie font-semibold">OPTmander</p><p className="3xl:text-4xl 2xl:text-3xl  text-md sm:text-xl font-Nixie font-semibold">No</p></div>
+        <div className="  -top-5  text-gray-600 font-serif flex flex-col 3xl:w-108 2xl:w-80 xl:w-60 3xl:h-36 2xl:h-28 xl:h-20 sm:w-56 w-36 absolute items-center justify-evenly sm:h-16 h:10 sm:mt-8 mt-9"><p className="3xl:text-4xl 2xl:text-3xl  text-md sm:text-xl font-Nixie font-semibold">OPTmander</p><p className="3xl:text-4xl 2xl:text-3xl  text-md sm:text-xl font-Nixie font-semibold">No</p></div>
       </div>
 
       <div className=" sm:w-2/5 lg:w-1/5 w-full m-auto flex justify-evenly "><a href="https://twitter.com/wearecultdao" target="_blank" className="hover:animate-bounce" rel="noreferrer">
